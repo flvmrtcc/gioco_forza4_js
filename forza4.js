f6df891aec3ba522ceb6d109c6e6ceafbc62d1bc
flvmrtcc <flavio.martucci.fm@gmail.com>
2021-07-04 20:40:53 +0200

    inserimento e controlli pedine gioco

--- forza4.js
+++ forza4.js
@@ -6,7 +6,7 @@ function Forza4() {
         [0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 2, 0, 0, 0]
+        [0, 0, 0, 0, 0, 0, 0]
     ];
     let numeroRighe = 6;
     let numeroColonne = 7;
@@ -28,25 +28,56 @@ function Forza4() {
         document.getElementById("contenitoreGioco").innerHTML += "<div id='schermoInfoPartita'><h2><br></h2></div>"
         document.getElementById("contenitoreGioco").innerHTML += `<div id="divGioco">${strTab}</div>`;
         assegnaOnclickCelle();
+        stringaGiocatoreCorrente();
     }
 
     let stringaGiocatoreCorrente = function () {
+        let colore;
+        if (utenteCorrente == 1)
+            colore = "red";
+        else
+            colore = "yellow";
         document.getElementById("schermoInfoPartita").getElementsByTagName("h2")[0].innerHTML = `Turno del giocatore ${utenteCorrente}`;
+        document.getElementById("schermoInfoPartita").getElementsByTagName("h2")[0].style = `color: ${colore}`;
     }
 
     let controllaCellaCliccata = function (cellaCliccata) {
-        // console.log(cellaCliccata.id);
         let str = cellaCliccata.id;
-        // console.log(str.split("_").pop());      // rimuove "cella_" e tiene solo il numero della cella
         str = str.substring('cella_'.length);      // rimuove "cella_" e tiene solo il numero della cella
         let numeroCellaCliccata = parseInt(str);
         let rigaCellaCliccata = parseInt(numeroCellaCliccata / numeroColonne);
         let colonnaCellaCliccata = numeroCellaCliccata % numeroColonne;
-        console.log(`Colonna= ${colonnaCellaCliccata}, Riga= ${rigaCellaCliccata}`);
-        matrice[rigaCellaCliccata][colonnaCellaCliccata] = utenteCorrente;
-        mostraCella(numeroCellaCliccata, rigaCellaCliccata, colonnaCellaCliccata);
-        utenteSuccessivo();
+
+        if (cellaVuota(rigaCellaCliccata, colonnaCellaCliccata)) {
+            posizionaNellaRigaPiuBassaLibera(colonnaCellaCliccata);
+            // matrice[rigaCellaCliccata][colonnaCellaCliccata] = utenteCorrente;
+            // mostraCella(numeroCellaCliccata, rigaCellaCliccata, colonnaCellaCliccata);
+            forza4.mostraCelle();
+            utenteSuccessivo();
+
+        }
+
     }
+
+    let cellaVuota = function (rigaCellaCliccata, colonnaCellaCliccata) {
+        if (matrice[rigaCellaCliccata][colonnaCellaCliccata] == 0)
+            return true;
+        return false;
+    }
+
+    let posizionaNellaRigaPiuBassaLibera = function (colonnaCellaCliccata) {
+        let cellaOccupata = true;
+        let r = numeroRighe - 1;
+        do {
+            if (matrice[r][colonnaCellaCliccata] == 0) {
+                cellaOccupata = false;
+                matrice[r][colonnaCellaCliccata] = utenteCorrente;
+            }
+            else r -= 1;
+            // if (r < 0) cellaOccupata = false;
+        } while (cellaOccupata == true);
+    }
+
 
     let utenteSuccessivo = function () {
         if (utenteCorrente == 1)
